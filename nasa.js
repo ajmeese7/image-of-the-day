1406c2c7eb55066d766f760e6ea0ac2fea240aa6
Aaron Meese <ajmeese7@gmail.com>
2018-11-11 20:41:46 -0600

    NASA works
    Finally got my dynamic-page-retrieval working, so I made this work pretty easily.

--- nasa.js
+++ nasa.js
@@ -1,31 +1,18 @@
 function nasa() {
-  var url = "https://www.nasa.gov/multimedia/imagegallery/iotd.html";
-
-  /*$.ajax({
-    url: url,
-    success: function(result) {
-      var image = document.getElementById("image");
-      //image.src = result;
-      image.height = screen.height;
-    }
-  });*/
-
-  var source = "https://www.nasa.gov/sites/default/files/styles/full_width_feature";
+  var url = "http://dynamic-page-retrieval.herokuapp.com/scrape?URL=https://www.nasa.gov/multimedia/imagegallery/iotd.html";
 
   // Get HTML from page
-  $.get(url, function(html) {
-    // Loop through elements you want to scrape content from
-    var code = $.parseHTML(html);
-    console.log(html);
-    console.log(code);
+  $.get(url, function(res) {
+      // Loop through elements you want to scrape content from
+      var gallery = $(res.html).find("#gallery-list");
+      var imageOfDay = $(gallery).children(0).children(0).children(0).children(0).children(0)[0];
 
-    $(html).find("#gallery-list").children(".image").first().each(function() {
-      var source = "https://www.nasa.gov";
-      source += $(this).attr("src");
+      var source = $(imageOfDay).attr("src");
+      source = source.slice(source.indexOf("image/") + 6); // Gets everything after "image/" in the URL
+      source = "https://www.nasa.gov/sites/default/files/thumbnails/image/" + source; // Makes it show the full res image
 
       var image = document.getElementById("image");
       image.src = source;
       image.height = screen.height;
-    })
   });
 }
